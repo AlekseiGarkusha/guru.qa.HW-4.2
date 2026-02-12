36df4a76cd65dd85a0ce656af348668edac2edd7
Alex <sapphire6061@gmail.com>
2026-02-12 23:14:21 +0300

    Add test - Positive Test

--- src/test/java/PositiveTest.java
+++ src/test/java/PositiveTest.java
@@ -1,9 +1,11 @@
-import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.Test;
 import setup.Base;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$x;
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class PositiveTest extends Base {
 
@@ -15,20 +17,34 @@ public class PositiveTest extends Base {
     $x("//input[@id='userEmail' and @placeholder='name@example.com']").sendKeys("test@ya.ru");
 
     $x("//input[@id='gender-radio-1' and @value='Male']/following-sibling::label").click();
-    $x("//input[@id='userNumber' and @placeholder='Mobile Number']").sendKeys("123456789");
+    $x("//input[@id='userNumber' and @placeholder='Mobile Number']").sendKeys("1234567890");
 
-//    SelenideElement dateOfBirth = $x("//input[@id='dateOfBirthInput' and @class='form-control']");
-//    dateOfBirth.clear();
-////    dateOfBirth.sendKeys("12.02.2000");
+    $x("//input[@id='dateOfBirthInput' and @class='form-control']").click();
 
+    $x("//select[contains(@class,'react-datepicker__month-select')]/option[text()='March']").click();
+    $x("//select[contains(@class,'react-datepicker__year-select')]/option[text()='1991']").click();
+
+    $x(
+      "//div[contains(@class,'react-datepicker__day') " +
+        "and text()='29' " +
+        "and not(contains(@class,'outside-month'))]"
+    ).click();
+
+    $x("//input[@id='hobbies-checkbox-1' and @value='1']/following-sibling::label").click();
     $x("//input[@id='hobbies-checkbox-2' and @value='2']/following-sibling::label").click();
+    $x("//input[@id='hobbies-checkbox-3' and @value='3']/following-sibling::label").click();
 
+    File file = new File("src/test/resources/Code.png");
+    $("#uploadPicture").uploadFile(file);
 
-    $("#uploadPicture").uploadFile("=./build.gradle");
+    $("#currentAddress").sendKeys("Some fish text");
 
+    $x("//div[contains(@class, 'css-yk16xz-control') and .//div[text()='Select State']]").click();
+    $(byText("NCR")).shouldBe(visible).click();
 
+    $x("//div[contains(@class, 'css-yk16xz-control') and .//div[text()='Select City']]").click();
+    $(byText("Noida")).shouldBe(visible).click();
 
-
+    $("#submit").click();
   }
-
 }
